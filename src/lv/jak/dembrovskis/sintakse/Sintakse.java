90285c7e3cbba1362e94228337ec09c287ee8066
lietotajs <lietotajs@121-05>
2019-12-20 10:57:13 +0200

    Aprakstita klase sintakse

---
+++ src/lv/jak/dembrovskis/sintakse/Sintakse.java
@@ -0,0 +1,88 @@
+//komentars - programmsa koda daļa kas palidz programmas izstradatajiem labak izprast koda darbibu
+//principu. Programmas darbibas laika komentari laika komentari netik nemti vera
+
+
+//- viena srindas komentars
+/*
+* vairaku
+* rindu 
+* komentars
+* 
+*/
+
+
+
+package lv.jak.dembrovskis.sintakse;  //pakotne - klases atrašanas vieta projiekta
+//import komanda - komandrinda kas nodrošina citas klases
+//Tada veida tiek nodrošinatas dažadu funkciju izmantošanas
+//datu ievade no tastaturas
+/*
+ * iespejamas pierakstas formas
+
+ * import <pakotne>.<klase>
+ *  import <pakotne>.*
+ */
+import java.util.Scanner;
+
+//JAVA klases daļa - agllvena programešanas daļa
+//Klases nosaukums
+/*
+ * Sintakses nosacijums uz klases nosaukumu
+ * 		1.Klases nasaukums sakas ar lielo burtu
+ * 		2.Ja klases nosaukums sarut vairakus vardus, katrs vards sakas ar lielo burtu
+ * 		3.Nosaukums var saturet  TIKAI BURTUS un CIPARUS 
+ * 		4. Ja klase nosaukums satur vairakus vardus tos ar atstarpem NEATDALA!!! 
+ */
+
+
+
+public class Sintakse { 
+	
+	
+	//klases kermenis - šaja daļa aprakstits koda fragments ie pieejms visai klase
+	//izņemuma nasacijums - aprakstitas lietas ir redzams tikai tai daļa koda kas atrodas zamā!
+	//!!!Nedriks takstit funkcionalos procesus, izņemot vertibas pieškiršanu  vai objekta izveidi!
+	
+	//mainigie ir programešanas valodas pamatelements 
+	
+	 //Sintakses nosacijums uz mainiga nosaukumu
+	/*
+	 * 		1. mainiga nosaukums sakas ar mazo burtu
+	 * 		2. ja mainiga nosaukums satur vairakus vardus pirmais vards ar mazo, katrais nakamais ar lielo burtu
+	 * 		3.Ja mainiga nosaukums satur vairakus vardus tos ar atstarpem NEATDALAS!!! 
+	 * 		4.Nosaukums var saturet  TIKAI BURTUS un CIPARUS 
+	 * 
+	 * Izņemuma gadijumi:
+	 * 
+	 * 
+	 */
+//mainiga izveidošana
+	/*
+	 * pierakstu forma:
+	 * 		-->ja ieprikš mainigais NAV izveidots: <datuTips> <mainigaNosaukums>
+	 */
+	t veselsSkaitlis;
+	
+	
+	
+	//metode MAIN - galvena metodi. 
+	/*
+	 * MAIN metode:
+	 * 		1. Uz projiekru  jabut 	vismaz 1 MAIN metode
+	 * 		2. Metodes pierakts ir TIEŠI tads ka redzams zamak pieraksta 
+	 *
+	 */
+	
+	
+	public static void main(String[] args) {
+	} 
+			//metodes kermenis - aprakstam algoritmu kuru velamies ispildit 
+//Klase skermenis ir beidzies 
+}
+//!!! NEKO ŠEIT NEDRIKST RAKSTIT
+
+
+
+
+
+
